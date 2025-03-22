79400b94ebb47d7695386470f3bc9ba3410d7e24
Himanshu <himanshudhaila098@gmail.com>
2025-03-22 18:54:42 +0530

    This is the Complete lecture matter of lecture 1 and 2

---
+++ course learning concepts/lecture2_Conditionals/lecture 2,During lecture  easy mha_mini_Projects/funtry.cpp
@@ -0,0 +1,15 @@
+
+
+                            /* The in class tasks */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    if(0){
+        cout<<"Hello";
+    }else{
+        cout<<"Bye";
+    }
+    return 0;
+}
